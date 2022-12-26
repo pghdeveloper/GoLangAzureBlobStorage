4838e6ae24d0f23d0d37bc4fbe0010bf8f8361d0
pghdeveloper <rmardayat@gmail.com>
2022-12-26 16:07:04 -0500

    Added Config.Go

---
+++ util/config.go
@@ -0,0 +1,26 @@
+package util
+
+import (
+	"github.com/spf13/viper"
+)
+
+type Config struct {
+	AccountName string `mapstructure:"ACCOUNT_NAME"`
+	AccountKey string `mapstructure:"ACCOUNT_KEY"`
+}
+
+func LoadConfig(path string) (config Config, err error) {
+	viper.AddConfigPath(path)
+	viper.SetConfigName("app")
+	viper.SetConfigType("env")
+
+	viper.AutomaticEnv()
+
+	err = viper.ReadInConfig()
+	if err != nil {
+		return
+	}
+
+	err = viper.Unmarshal(&config)
+	return
+}
